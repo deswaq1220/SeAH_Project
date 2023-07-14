8c91a5fd52b042e97b10c69d10000498e2c1bfa4
suin <l3esde67@naver.com>
2023-07-14 11:51:29 +0900

    교육등록 엔티티 생성

--- savg/src/main/java/SeAH/savg/constant/edustate.java
+++ savg/src/main/java/SeAH/savg/constant/edustate.java
@@ -2,5 +2,15 @@ package SeAH.savg.constant;
 
 public enum edustate {
 
+    //교육 분류
+    A, //집체교육
+    B, //안전조회
+    C, //관리감독
+
+
+    //교육대상 분류
+    T,  //total 전체
+    F, //filed 현장
+    O //office 사무직
 
 }
--- savg/src/main/java/SeAH/savg/entity/Edu.java
+++ savg/src/main/java/SeAH/savg/entity/Edu.java
@@ -1,7 +1,14 @@
 package SeAH.savg.entity;
 
-import javax.persistence.*;
+import SeAH.savg.constant.edustate;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 @Entity
 public class Edu {
 
@@ -9,7 +16,31 @@ public class Edu {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eduID;
 
+    @Column(nullable = false)
+    private edustate eduCategory;
 
+    @Column(nullable = false)
+    private String instructor;
 
+    @Column(nullable = false)
+    private String eduPlace;
+
+    @Column(nullable = false)
+    private LocalDateTime eduStartTime;
+
+    @Column(nullable = false)
+    private LocalDateTime eduEndTime;
+
+    @Column(nullable = false)
+    private Integer eduSumTime;
+
+    @Column(nullable = false)
+    private edustate eduTarget;
+
+    @Column(nullable = false)
+    private String eduContent;
+
+    @Column(nullable = false)
+    private String eduWriter;
 
 }
