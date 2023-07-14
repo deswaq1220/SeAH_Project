d8885ed9ee389b9047f648ed52ab9b7cc56f1be9
deswaq789 <deswaq789@naver.com>
2023-07-14 17:22:27 +0900

    이메일보내기

---
+++ savg/src/main/java/SeAH/savg/controller/EmailController.java
@@ -0,0 +1,46 @@
+package SeAH.savg.controller;
+import SeAH.savg.Email.EmailData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@RestController
+public class EmailController {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailController(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/api/send-email")
+    public ResponseEntity<Object> sendEmail(@RequestBody EmailData emailData) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(emailData.getRecipient());
+            helper.setSubject(emailData.getSubject());
+            helper.setText(emailData.getContent());
+
+            mailSender.send(message);
+            return ResponseEntity.ok().body(new ResponseMessage("Email sent successfully."));
+        } catch (MessagingException e) {
+            e.printStackTrace();
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending email.");
+        }
+    }
+
+
+
+}
