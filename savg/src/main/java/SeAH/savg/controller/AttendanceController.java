6d889b1c03c6f13db1f56f4392964c66306c800a
deswaq789 <deswaq789@naver.com>
2023-07-19 16:56:23 +0900

    출석

--- savg/src/main/java/SeAH/savg/controller/AttendanceController.java
+++ savg/src/main/java/SeAH/savg/controller/AttendanceController.java
@@ -2,14 +2,18 @@ package SeAH.savg.controller;
 
 import SeAH.savg.service.AttendanceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/usereduatten")
-//@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://172.20.10.5:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AttendanceController {
 
     private final AttendanceService attendanceService;
@@ -23,14 +27,14 @@ public class AttendanceController {
 
     //교육생 출석등록하기
     @PostMapping("/register")
-    public String registerAttendEdu(Model model
-                                   ,@RequestParam("attenDepartment") String attenDepartment
-                                   ,@RequestParam("attenName") String attenName
-                                   ,@RequestParam("attenEmployeeNumber") String attenEmployeeNumber) {
+    public ResponseEntity<?> registerAttendEdu(@RequestBody Map<String, String> requestData) {
+        String attenDepartment = requestData.get("attenDepartment");
+        String attenName = requestData.get("attenName");
+        String attenEmployeeNumber = requestData.get("attenEmployeeNumber");
 
         attendanceService.attendEdu(attenDepartment, attenName, attenEmployeeNumber);
 
-        return "page/attendance";
+        return ResponseEntity.ok().build();
     }
 
-}
+    }
